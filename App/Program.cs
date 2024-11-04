8fb80e847aba035debb8311a658f6fd08bf66cac
ottotirronen <otto.tirronen@edu.karelia.fi>
2024-11-04 23:43:18 +0200

    first commit

---
+++ App/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AppLogger;
+using Humanizer;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // humanisoidaan tekstiä
+            string text = "PascalCaseText";
+            Console.WriteLine(text.Humanize());
+
+            // Humanisoidaan aika
+            TimeSpan time = TimeSpan.FromDays(3);
+            Console.WriteLine(time.Humanize());
+            
+        }
+    }
+}
---
+++ AppLogger/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace AppLogger
+{
+    public class Logger
+    {
+        public static void Log(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
+
